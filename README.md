38df3e79dfa4b63bbbba4debeff27f189603d4f9
Jason Brackins <jason.brackins@gmail.com>
2016-09-16 18:03:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# JasonBrackins.com
+
+
+## Run Locally
+
+1. `npm install`
+2. `gulp watch`
+3. `firebase serve`
+
+
+## Goals
+
+* Introduce me professionally
+* But casually
+* Link to business and select persoanl sites
+* link to resume and portfolio
+
+### Audience
+
+* potential employers
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      pug  = require('gulp-pug')
+      // watch = require('gulp-watch')
+;
+
+gulp.task('clean', [], function() {
+
+});
+
+gulp.task('sass', ['clean'], function() {
+  return gulp.src('src/jb.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('pug', ['clean'], function() {
+  return gulp.src('src/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('assets', ['clean'], function() {
+  return gulp.src('src/assets/**/*')
+    .pipe(gulp.dest('public/assets'));
+});
+
+gulp.task('watch', ['build'], function () {
+  return gulp.watch('src/**/*', ['build']);
+});
+
+gulp.task('build', ['sass', 'pug', 'assets']);
+gulp.task('default', ['build']);
